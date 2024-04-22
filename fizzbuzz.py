65f65e13b7369f7dd33662bec0d07e8024b8f286
narykkim <narykkim@naver.com>
2024-04-22 11:51:14 +0900

    feat: Add print fizzbuzz if i % 15 == 0
    on branch fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,9 @@
 for i in range(1,16):
-    if i % 5 == 0:
+    if i % 15 == 0:
+        print("fizzbuzz")
+    elif i % 5 == 0:
         print("buzz")
+    elif i % 3 == 0:
+        print("fizz")
+    else:
+        print(i)
